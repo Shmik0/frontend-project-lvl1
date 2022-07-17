f8a13642f5d85c19d58bfb5324b9cbb66f2e52c1
Shmiko <zaitcev87@gmail.com>
2022-07-17 11:01:55 +0300

    added new folder games

---
+++ bin/games/brain-even.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
+const userName = readlineSync.question('May I have your name? ');
+
+console.log('Welcome to the Brain Games!');
+console.log(`Hello, ${userName}!`);
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+const getRandomNum = (max) => Math.floor(Math.random() * max);
+
+const evenOrOdd = () => {
+  for (let i = 0; i < 3; i += 1) {
+    const number = getRandomNum(100);
+    console.log(`Question: ${number}`);
+    const answer = readlineSync.question('Your answer: ');
+    const result = number % 2 === 0 ? 'yes' : 'no';
+    if (result === answer) {
+      console.log('Correct!');
+    } else {
+      return `'${answer}' is wrong answer ;(. Correct answer was '${result}'.\nLet's try again, ${userName}!`;
+    }
+  }
+  return `Congratulations, ${userName}!`;
+};
+console.log(evenOrOdd(getRandomNum));
