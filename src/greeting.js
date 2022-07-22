86b4b7163b9105b202229907ba370bcaf5f6299d
Shmiko <zaitcev87@gmail.com>
2022-07-22 19:27:20 +0300

    deleted greeting.js

--- src/greeting.js
+++
@@ -1,7 +0,0 @@
-import readlineSync from 'readline-sync';
-
-const userName = readlineSync.question('May I have your name? ');
-
-const greeting = (`Welcome to the Brain Games! \nHello, ${userName}!`);
-
-export { greeting, userName };
