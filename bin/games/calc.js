9b05c2b3c1bb571d09fa8fce9cf74ca74552a06f
Shmiko <zaitcev87@gmail.com>
2022-07-22 20:57:24 +0300

    fix

--- bin/games/calc.js
+++ bin/games/calc.js
@@ -1,28 +1,28 @@
 import basis from '../../src/index.js';
 import { getRandomValue, getRandomOperator, operators } from '../../src/randomValue.js';
 
+const calcTask = () => {
+  const number1 = getRandomValue(100);
+  const number2 = getRandomValue(100);
+  const operator = getRandomOperator(operators);
+
+  const question = `${number1} ${operator} ${number2}`;
+
+  let result = 0;
+  if (operator === '+') {
+    result = number1 + number2;
+  }
+  if (operator === '-') {
+    result = number1 - number2;
+  }
+  if (operator === '*') {
+    result = number1 * number2;
+  }
+  return [question, result];
+};
+
 const calc = () => {
   const calcNote = 'What is the result of the expression?';
-
-  const calcTask = () => {
-    const number1 = getRandomValue(100);
-    const number2 = getRandomValue(100);
-    const operator = getRandomOperator(operators);
-
-    const question = `${number1} ${operator} ${number2}`;
-
-    let result = 0;
-    if (operator === '+') {
-      result = number1 + number2;
-    }
-    if (operator === '-') {
-      result = number1 - number2;
-    }
-    if (operator === '*') {
-      result = number1 * number2;
-    }
-    return [question, result];
-  };
   basis(calcNote, calcTask);
 };
 export default calc;
