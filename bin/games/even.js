8cca0e64ee95c4c5b14399cbe0f4894fa7eb10e9
Shmiko <zaitcev87@gmail.com>
2022-07-22 20:54:54 +0300

    renew

--- bin/games/even.js
+++ bin/games/even.js
@@ -1,16 +1,16 @@
 import basis from '../../src/index.js';
 import { getRandomValue } from '../../src/randomValue.js';
 
+const evenTask = () => {
+  const number = getRandomValue(100);
+  const question = number;
+
+  const result = number % 2 === 0 ? 'yes' : 'no';
+  return [question, result];
+};
+
 const even = () => {
   const evenNote = 'Answer "yes" if the number is even, otherwise answer "no".';
-
-  const evenTask = () => {
-    const number = getRandomValue(100);
-    const question = number;
-
-    const result = number % 2 === 0 ? 'yes' : 'no';
-    return [question, result];
-  };
   basis(evenNote, evenTask);
 };
 
