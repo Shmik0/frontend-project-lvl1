bf4c34a3bd58629b64493876c162fbeaa6c24630
Shmiko <zaitcev87@gmail.com>
2022-07-25 17:15:01 +0300

    simplified code

--- bin/games/gcd.js
+++ bin/games/gcd.js
@@ -7,16 +7,10 @@ const gcdTask = () => {
   const question = `${number1} ${number2}`;
 
   let result = 0;
-  for (let i = 1; i <= number1 / 2; i += 1) {
+  for (let i = 1; i <= number1; i += 1) {
     if (number1 % i === 0 && number2 % i === 0) {
       result = i;
     }
-  }
-  if (number2 % number1 === 0) {
-    result = number1;
-  }
-  if (number1 % number2 === 0) {
-    result = number2;
   }
   return [question, result];
 };
