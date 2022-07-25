e747ab5dfcc081cf2467638655b7649959a70cb8
Shmiko <zaitcev87@gmail.com>
2022-07-25 19:48:08 +0300

    added new formula

--- src/randomValue.js
+++ src/randomValue.js
@@ -1,7 +1,10 @@
 const getRandomValue = (max) => Math.floor(Math.random() * max);
+const getRandomOfRange = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
 const operators = ['+', '-', '*'];
 const indexOfOperator = (min, max) => Math.floor(Math.random() * (max - min) + min);
 const getRandomOperator = () => operators[indexOfOperator(0, 3)];
 
-export { getRandomValue, getRandomOperator, operators };
+export {
+  getRandomValue, getRandomOfRange, getRandomOperator, operators,
+};
