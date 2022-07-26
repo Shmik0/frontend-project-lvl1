52f89ce0381db7ce35caffaa6fd1bf3e90d0b200
Shmiko <zaitcev87@gmail.com>
2022-07-26 15:57:09 +0300

    added new game brain-progression

---
+++ bin/games/prime.js
@@ -0,0 +1,22 @@
+import basis from '../../src/index.js';
+import { getRandomValue } from '../../src/randomValue.js';
+
+const primeTask = () => {
+  const number = getRandomValue(100);
+  const question = number;
+
+  let result = 'yes';
+  for (let i = 2; i <= number / 2; i += 1) {
+    if (number % i === 0) {
+      result = 'no';
+    }
+  }
+  return [question, result];
+};
+
+const prime = () => {
+  const primeNote = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+  basis(primeNote, primeTask);
+};
+
+export default prime;
